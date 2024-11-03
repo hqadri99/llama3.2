fdaf6f99190b705ec46707159f80a1809e42dfdf
Husnain Rasul Qadri <hqadri@usc.edu>
2024-11-03 04:34:51 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -20,7 +20,7 @@ image = Image.open(requests.get(url, stream=True).raw)
 messages = [
     {"role": "user", "content": [
         {"type": "image"},
-        {"type": "text", "text": "Can you tell coordinates of Chocoloate Cone?."}
+        {"type": "text", "text": "Can you tell coordinates of Pizza?"}
     ]}
 ]
 input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
