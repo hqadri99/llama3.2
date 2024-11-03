5c0a061759a141bc1104e985b0b04cb098bf3679
Husnain Rasul Qadri <hqadri@usc.edu>
2024-11-02 20:25:14 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -20,7 +20,7 @@ image = Image.open(requests.get(url, stream=True).raw)
 messages = [
     {"role": "user", "content": [
         {"type": "image"},
-        {"type": "text", "text": "What are the coordinates of blue squares?"}
+        {"type": "text", "text": "Can you share locations of the green ships?"}
     ]}
 ]
 input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
