91266fe5a976850b5a18c638eee9510e1c1bc4a4
Husnain Rasul Qadri <hqadri@usc.edu>
2024-11-23 15:23:53 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -38,6 +38,7 @@
 import requests
 import torch
 from PIL import Image
+from transformers import BitsAndBytesConfig
 from transformers import MllamaForConditionalGeneration, AutoProcessor
 
 model_id = "meta-llama/Llama-3.2-90B-Vision-Instruct"
@@ -50,11 +51,17 @@ hf_token = "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"
 #     use_auth_token=hf_token,
 # )
 
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,  # Enable 4-bit quantization
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,  # Use BF16 for computations
+)
+
 model = MllamaForConditionalGeneration.from_pretrained(
     model_id,
     torch_dtype=torch.bfloat16,
-    device_map="auto",  # Automatically distribute across GPUs
-    offload_folder="./offload",  # CPU/disk offloading
+    quantization_config=bnb_config,
+    device_map="auto",
     use_auth_token=hf_token,
 )
 
