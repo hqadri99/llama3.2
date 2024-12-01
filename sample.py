9c5db6bb385615eebc3f97adbb165ea240bf3cee
Husnain Rasul Qadri <hqadri@usc.edu>
2024-12-01 01:23:13 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -44,29 +44,29 @@ from transformers import MllamaForConditionalGeneration, AutoProcessor
 model_id = "meta-llama/Llama-3.2-90B-Vision-Instruct"
 hf_token = "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"
 
-model = MllamaForConditionalGeneration.from_pretrained(
-    model_id,
-    torch_dtype=torch.bfloat16,
-    device_map="auto",
-    use_auth_token=hf_token,
-)
-
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,  # Enable 4-bit quantization
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_compute_dtype=torch.bfloat16,  # Use BF16 for computations
-# )
-
 # model = MllamaForConditionalGeneration.from_pretrained(
 #     model_id,
 #     torch_dtype=torch.bfloat16,
-#     quantization_config=bnb_config,
 #     device_map="auto",
 #     use_auth_token=hf_token,
 # )
 
-# # Tie weights explicitly
-# model.tie_weights()
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,  # Enable 4-bit quantization
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,  # Use BF16 for computations
+)
+
+model = MllamaForConditionalGeneration.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    quantization_config=bnb_config,
+    device_map="auto",
+    use_auth_token=hf_token,
+)
+
+# Tie weights explicitly
+model.tie_weights()
 
 processor = AutoProcessor.from_pretrained(model_id, use_auth_token=hf_token)
 
