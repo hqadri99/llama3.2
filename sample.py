3da4a62a3199b9b19b907fb6a15aae2f134eccad
Husnain Rasul Qadri <hqadri@usc.edu>
2024-12-01 00:12:18 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -44,12 +44,12 @@ from transformers import MllamaForConditionalGeneration, AutoProcessor
 model_id = "meta-llama/Llama-3.2-90B-Vision-Instruct"
 hf_token = "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"
 
-# model = MllamaForConditionalGeneration.from_pretrained(
-#     model_id,
-#     torch_dtype=torch.bfloat16,
-#     device_map="auto",
-#     use_auth_token=hf_token,
-# )
+model = MllamaForConditionalGeneration.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+    use_auth_token=hf_token,
+)
 
 # bnb_config = BitsAndBytesConfig(
 #     load_in_4bit=True,  # Enable 4-bit quantization
